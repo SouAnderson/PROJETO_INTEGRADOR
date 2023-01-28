490aef49f8ac56f3e9ea298c9fd7301ca642f9c9
Anderson Santos <andersonadunar@gmail.com>
2023-01-28 06:20:20 -0300

    implementado o filtro da pag de produtos

--- webSiteIflag/produtos.js
+++ webSiteIflag/produtos.js
@@ -1,9 +1,82 @@
-let filtro = document.querySelectorAll(".menu li a");
+//Linha 2 à 82, códigos do filtro
+function mostrarCards(){
+    for(let card of container){
+        card.style.display = "block"
+    }
 
-
-for(let i = 0 ;i < filtro.length ; i++){
-    console.log(filtro[i].innerText);
+    removeFiltro.style.display = "none"
 }
 
+function filtraCard(itemClass){
+
+    for(let card of container){
+        let temClass = card.classList.contains(itemClass);
+        console.log(temClass)
+
+        if(!temClass){
+            card.style.display = "none" 
+        }
+        
+    }
+
+    removeFiltro.style.display = "block"
+}
+
+let filtro = document.querySelectorAll(".menu li p")
+let container = document.querySelectorAll("#teste section")
+let removeFiltro = document.querySelector("#remove-filtro")
 
 
+filtro[0].addEventListener("click", ()=> {
+
+    mostrarCards()
+    filtraCard("notebook")
+
+})
+
+
+filtro[1].addEventListener("click", ()=> {
+
+    mostrarCards()
+    filtraCard("teclado")
+
+})
+
+
+filtro[2].addEventListener("click", ()=> {
+    
+    mostrarCards()
+    filtraCard("mouse")
+
+})
+
+
+filtro[3].addEventListener("click", ()=> {
+
+    mostrarCards()
+    filtraCard("fone")
+
+})
+
+
+filtro[4].addEventListener("click", ()=> {
+
+    mostrarCards()
+    filtraCard("placa-video")
+
+})
+
+
+filtro[4].addEventListener("click", ()=> {
+
+    mostrarCards()
+    filtraCard("placa-video")
+
+})
+
+
+removeFiltro.addEventListener("click", ()=> {
+
+    mostrarCards()
+
+})
