6a27411bda0212797778d7ab14b16bdda05c27fa
Fernando HBS <fernandobailao@gmail.com>
2023-02-02 20:24:35 -0300

    UPDATED PROJECT

---
+++ iflag-site/scripts/app.js
@@ -0,0 +1,13 @@
+
+function send() {
+const datas = document.getElementById('search').value;
+if (datas === "") {
+alert('Por favor, preencha o campo...')
+} 
+else {
+}
+document.getElementById('search').value='' ; 
+}
+
+
+
