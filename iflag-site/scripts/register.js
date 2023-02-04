e05369ee1148a9c27bb15a179f63609f8ed09034
Fernando HBS <fernandobailao@gmail.com>
2023-02-03 23:55:37 -0300

    Update register.js

--- iflag-site/scripts/register.js
+++ iflag-site/scripts/register.js
@@ -12,21 +12,25 @@
       name.focus();
       return;
     }
+  
     if (email.value.length <= 10 || email.value == "") {
       alert("E-mail não informado.");
       email.focus();
       return;
     }
+  
     if (password.value.length <= 5 || password.value == "") {
       alert("Senha não informada. A senha deve ter mais do que 5 carateres.");
       password.focus();
       return;
     }
+  
     if (phone.value.length <= 11  || phone.value == "") {
       alert("Telefone não informado. Insira o DDD e o número do seu telefone.");
       phone.focus();
       return;
     }
+  
     alert("Sucesso! Você será redirecionado para a página de Login!");
     location.href = "/pages/login.html"
     return false;
