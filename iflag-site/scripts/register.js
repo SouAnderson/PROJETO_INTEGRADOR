52ac84169b5b9f42174371d4f29dbfc29db8a3f6
Fernando HBS <fernandobailao@gmail.com>
2023-02-05 13:21:32 -0300

    Update register.js

--- iflag-site/scripts/register.js
+++ iflag-site/scripts/register.js
@@ -1,36 +1,31 @@
-
- function 
+function 
  validate() {
 
-    var name = document.getElementById("name");
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    var phone = document.getElementById("phone");
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let phone = document.getElementById("phone");
     
-    if (name.value.length <= 2 || name.value == "") {
+    if (name.value == "" || name.value.length <= 2) {
       alert("Nome não informado. Preencha todos os campos para se cadastrar.");
       name.focus();
       return;
     }
-  
-    if (email.value.length <= 10 || email.value == "") {
-      alert("E-mail não informado.");
+    if (email.value == "" ||  email.value.length <= 10 || email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+      alert("E-mail inválido.");
       email.focus();
       return;
     }
-  
     if (password.value.length <= 5 || password.value == "") {
       alert("Senha não informada. A senha deve ter mais do que 5 carateres.");
       password.focus();
       return;
     }
-  
     if (phone.value.length <= 11  || phone.value == "") {
       alert("Telefone não informado. Insira o DDD e o número do seu telefone.");
       phone.focus();
       return;
     }
-  
     alert("Sucesso! Você será redirecionado para a página de Login!");
     location.href = "/pages/login.html"
     return false;
